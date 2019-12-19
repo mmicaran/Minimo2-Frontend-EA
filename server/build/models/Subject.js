29c5165e76df536ae7e5cc004d879cf8c9102f55
Adria <adri.navarrete.09@gmail.com>
2019-12-20 00:19:48 +0100

    modals students

--- server/build/models/Subject.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const SubjectSchema = new mongoose_1.Schema({
-    name: { type: String, required: true, unique: true },
-    students: [{ type: mongoose_1.Schema.Types.ObjectId, ref: 'Student', unique: false }]
-});
-exports.default = mongoose_1.model('Subject', SubjectSchema);
--- server/models/Subject.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const SubjectSchema = new Schema({
-    name: {type: String, required: true, unique: true},
-    students: [{type: Schema.Types.ObjectId, ref: 'Student', unique: false }]
-    
-});
-
-export default model('Subject', SubjectSchema);
